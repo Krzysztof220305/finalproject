063bb7637b9b6232361055eff455e9ed8643880f
Krzysztof <krzysiurr4@gmail.com>
2026-02-19 13:56:23 +0000

    11

--- inventory/urls.py
+++ inventory/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # To kieruje bezpośrednio do Twojej listy produktów
     path('', views.item_list, name='item_list'),
-    path('add/', views.item_create, name='item_create'),
-    path('update/<int:id>/', views.item_update, name='item_update'),
-    path('delete/<int:id>/', views.item_delete, name='item_delete'),
 ]

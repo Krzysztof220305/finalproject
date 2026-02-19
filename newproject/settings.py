a0fa8e1387ecf0f094df29a15e25d26e9ca493ae
Krzysztof <krzysiurr4@gmail.com>
2026-02-19 13:03:08 +0000

    3

--- newproject/settings.py
+++ newproject/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 import os
 from pathlib import Path
-import dj_database_url  # Library for parsing database URLs
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
